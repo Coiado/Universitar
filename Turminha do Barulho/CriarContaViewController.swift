ac52dc4a53e5ecc608c94ad62f582b21e2ff009b
Bruno Eiji <brunoeijioy@gmail.com>
2015-10-25 02:32:47 -0200

    Criado as classes do login view controller e do criar conta, falta colocar coisas lá AHHAHA

---
+++ Turminha do Barulho/CriarContaViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CriarContaViewController.swift
+//  Turminha do Barulho
+//
+//  Created by Bruno Eiji Yoshida on 23/10/15.
+//  Copyright © 2015 Lucas Coiado Mota. All rights reserved.
+//
+
+import UIKit
+
+class CriarContaViewController : UIViewController {
+    
+    
+    
+    
+}
---
+++ Turminha do Barulho/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Turminha do Barulho
+//
+//  Created by Bruno Eiji Yoshida on 23/10/15.
+//  Copyright © 2015 Lucas Coiado Mota. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    
+}
