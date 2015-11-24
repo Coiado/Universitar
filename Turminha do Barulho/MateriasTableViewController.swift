cf3f235daaf24cd80247f37fc437bf3babc49216
Lucas Coiado Mota <lucascoiado55@gmail.com>
2015-11-24 12:14:45 -0200

    git pediu

--- Turminha do Barulho/MateriasTableViewController.swift
+++ Turminha do Barulho/MateriasTableViewController.swift
@@ -18,8 +18,16 @@ class MateriasTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //setando o navigation bar
+        self.navigationController?.navigationBar.barTintColor = UIColor.blackColor()
+        self.navigationController?.navigationBar.tintColor = UIColor.init(red: 255/255, green: 204/255, blue: 51/255, alpha: 1)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.init(red: 255/255, green: 204/255, blue: 51/255, alpha: 1.0) ]
         
-        //Dados em hardcode sendo carregados de um arquivo plist
+        //setando a tableview
+        
+        self.tableView.backgroundColor = UIColor.blackColor()
+        self.tableView.tableFooterView = UIView(frame:CGRectZero)
+        
         materias = Materia.loadAllMateria()
         
         // Uncomment the following line to preserve selection between presentations
