07b4cb78cdb2b9ee7d40a34481a6e1f585ef4389
Henrique Amitay <henriqueamitay@gmail.com>
2015-11-27 11:43:05 -0200

    TableView dos detalhes da noticia

--- Turminha do Barulho/FeedDetailsViewController.swift
+++ Turminha do Barulho/FeedDetailsViewController.swift
@@ -8,19 +8,31 @@
 
 import UIKit
 
-class FeedDetailsViewController: UIViewController {
+class FeedDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var navigationBarTitle: UINavigationBar!
-    @IBOutlet weak var subTitle: UILabel!
-    @IBOutlet weak var fullText: UITextView!
+    //TableView
+    @IBOutlet weak var detailsTableView : UITableView!
     
+    //Dados das noticias, devemos usar para mandar para nossa TableView
     var passedCell: Dados!
+    
+    //Vetor com os comentarios
+    var commentArray: [AnswerTableViewCell] = []
+    
+    //Celula da noticia
+    var newsCell : NewsDetailCell!
+    
+
     
     override func viewDidLoad() {
         
         self.navigationController?.navigationBarHidden = false
         
+        self.detailsTableView.registerNib(UINib(nibName: "DetalhesNoticiaCell", bundle: nil), forCellReuseIdentifier: "detailsCell")
+        
+        
+        self.detailsTableView.reloadData()
+        /*
         image.image = passedCell.imagem
         fullText.text = passedCell.fulltext
         subTitle.text = passedCell.subtitulo
@@ -28,9 +40,21 @@ class FeedDetailsViewController: UIViewController {
         
         fullText.layer.cornerRadius = 15
         fullText.layer.borderWidth = 3
+        */
+        self.detailsTableView.estimatedRowHeight = 100;
+        self.detailsTableView.rowHeight = UITableViewAutomaticDimension
         
     }
     
+    
+    func populateNewsDetails(cell: NewsDetailCell!)
+    {
+        cell.imagem.image = self.passedCell.imagem
+        cell.categoriaTitle.text = self.passedCell.titulo
+        cell.subTitle.text = self.passedCell.subtitulo
+        cell.fullText.text = self.passedCell.fulltext
+        //Metodo de resolucao da celula, TO DO
+    }
     
     func receiveCellData(cell: Dados) {
         self.passedCell = cell;
@@ -45,7 +69,33 @@ class FeedDetailsViewController: UIViewController {
         
     }
     
+    //TableView Delegate
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Noticia + comentarios
+        return 1 + self.commentArray.count
+    }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        //Muda de identifier para identifier
+        return 0;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //Precisamos retornar uma celula de noticia caso seja a primeira celula ou celulas de comentarios
+        let cell = tableView.dequeueReusableCellWithIdentifier("detailsCell", forIndexPath: indexPath) as! NewsDetailCell
+        
+        cell.imagem.image = self.passedCell.imagem
+        cell.categoriaTitle.text = self.passedCell.titulo
+        cell.subTitle.text = self.passedCell.subtitulo
+        cell.fullText.text = self.passedCell.fulltext
+        
+        
+        return cell //A priori
+    }
     
 }
