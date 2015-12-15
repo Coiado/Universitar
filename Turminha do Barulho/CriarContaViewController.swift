9d68363e75bd4ca2afe08973f07b320e4d2a4a92
Bruno Eiji <brunoeijioy@gmail.com>
2015-12-15 15:54:06 -0200

    Login e cadastro funcionando, falta apenas o error handler...

--- Turminha do Barulho/CriarContaViewController.swift
+++ Turminha do Barulho/CriarContaViewController.swift
@@ -7,10 +7,89 @@
 //
 
 import UIKit
+import Parse
 
-class CriarContaViewController : UIViewController {
+class CriarContaViewController : UIViewController, UITextFieldDelegate {
     
     
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var nomeCheck: UIImageView!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordCheck: UIImageView!
+    
+    @IBOutlet weak var confirmationTextField: UITextField!
+    @IBOutlet weak var confirmationCheck: UIImageView!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailCheck: UIImageView!
+    
+    @IBOutlet weak var confirmaButton: UIButton!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        
+        configureButton()
+        
+        self.activityIndicator.hidesWhenStopped = true
+        
+    }
+
+    func configureButton(){
+        
+        self.confirmaButton.addTarget(self, action: "confirmaAction", forControlEvents: UIControlEvents.TouchUpInside)
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.nomeTextField.resignFirstResponder()
+        self.passwordTextField.resignFirstResponder()
+        self.confirmationTextField.resignFirstResponder()
+        self.emailTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func confirmaAction(){
+        
+        let nome = nomeTextField.text!
+        let senha = passwordTextField.text!
+        let confirmacao = confirmationTextField.text!
+        let email = emailTextField.text!
+        
+        self.activityIndicator.startAnimating()
+        let user = PFUser()
+        user.username = nome
+        user.password = senha
+        user.email = email
+        
+        user.signUpInBackgroundWithBlock { (Bool, ErrorType) -> Void in
+            
+            self.activityIndicator.stopAnimating()
+            
+            if ErrorType == nil{
+                self.performSegueWithIdentifier("configuração", sender: self)
+            }
+            else{
+                print("ERROR CADASTRO")
+            }
+            
+        }
+        
+    }
     
     
 }
+
+
+
+
+
+
+
+
+
