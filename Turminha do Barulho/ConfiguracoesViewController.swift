99aa1b193f472bf1e728d25a8acfa42d447cc62a
Bruno Eiji <brunoeijioy@gmail.com>
2016-01-13 17:06:52 -0200

    Função para salvar a foto, e comprimir

--- Turminha do Barulho/ConfiguracoesViewController.swift
+++ Turminha do Barulho/ConfiguracoesViewController.swift
@@ -93,7 +93,69 @@ class ConfigViewController: UIViewController, UITableViewDataSource,UITableViewD
         
     }
     
+    func saveImage(){
+        
+//        let imageData = UIImagePNGRepresentation(self.profilePic.image)
+//        
+//        let image = UIImage(data: imageData)
+//        
+//        let compressedImage = compressImage(image!)
+//        
+//        let picture = PFFile(name: "image.png", data: compressedImage)
+//        
+//        PFUser.currentUser()!["foto"] = picture
+//        
+//        PFUser.currentUser()!.saveInBackgroundWithBlock {
+//            (success: Bool, error: NSError?) -> Void in
+//            if (success) {
+//                print("image has been saved")
+//            } else {
+//                print("image failed")
+//            }
+//        }
+        
+        
+    }
     
+    func compressImage(image:UIImage) -> NSData {
+        // Reducing file size to a 10th
+        
+        var actualHeight : CGFloat = image.size.height
+        var actualWidth : CGFloat = image.size.width
+        let maxHeight : CGFloat = 1136.0
+        let maxWidth : CGFloat = 640.0
+        var imgRatio : CGFloat = actualWidth/actualHeight
+        let maxRatio : CGFloat = maxWidth/maxHeight
+        let compressionQuality : CGFloat = 0.5
+        
+        if (actualHeight > maxHeight || actualWidth > maxWidth){
+            if(imgRatio < maxRatio){
+                //adjust width according to maxHeight
+                imgRatio = maxHeight / actualHeight;
+                actualWidth = imgRatio * actualWidth;
+                actualHeight = maxHeight;
+            }
+            else if(imgRatio > maxRatio){
+                //adjust height according to maxWidth
+                imgRatio = maxWidth / actualWidth;
+                actualHeight = imgRatio * actualHeight;
+                actualWidth = maxWidth;
+            }
+            else{
+                actualHeight = maxHeight;
+                actualWidth = maxWidth;
+            }
+        }
+        
+        let rect = CGRectMake(0.0, 0.0, actualWidth, actualHeight);
+        UIGraphicsBeginImageContext(rect.size);
+        image.drawInRect(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext();
+        let imageData = UIImageJPEGRepresentation(img, compressionQuality);
+        UIGraphicsEndImageContext();
+        
+        return imageData!;
+    }
     
 }
 
