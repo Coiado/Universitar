eacc30961922223552ea085d0a0f9e2e09b87fa4
Henrique Amitay <henriqueamitay@gmail.com>
2016-01-21 15:31:13 -0200

    Tableview de detalhes da resposta

--- Turminha do Barulho/QuestionFeedCell.swift
+++ Turminha do Barulho/QuestionFeedCell.swift
@@ -83,6 +83,7 @@ class QuestionFeedCell: UITableViewCell {
         self.perguntaTitulo.textColor = UIColor.whiteColor()
         self.questionText.textColor = UIColor.whiteColor()
         self.dateLabel.textColor = UIColor.whiteColor()
+        self.nickName.sizeToFit()
 
         
 
